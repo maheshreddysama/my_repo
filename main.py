a896b0e5d1229b5b9c246ce26ce8731d59bc1f21
maheshreddysama <samamahesh0079@gmail.com>
2025-05-22 20:18:26 -0400

    feat: split dataset

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
 from seaborn import load_dataset
 df = load_dataset('penguins')
 print(df.head())
+from sklearn.model_selection import train_test_split
+X = df.drop('species', axis=1)
+y = df['species']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
